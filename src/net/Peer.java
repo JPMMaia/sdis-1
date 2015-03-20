d5cde7d2f86ea284092d5c6ca3f21b891087a46d
jpmmaia <jpmmaia@gmail.com>
2015-03-20 15:43:52 +0000

    Initialing project. Adding gitignore.

---
+++ src/net/Peer.java
@@ -0,0 +1,24 @@
+package net;
+
+/**
+ * Created by João on 20/03/2015.
+ */
+public class Peer
+{
+    public Peer()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        String mcMulticastAddress = args[0];
+        String mcPort = args[1];
+        String mdbMulticastAddress = args[2];
+        String mdbPort = args[3];
+        String mdrMulticastAddress = args[4];
+        String mdrPort = args[5];
+
+        Peer peer = new Peer();
+    }
+}
